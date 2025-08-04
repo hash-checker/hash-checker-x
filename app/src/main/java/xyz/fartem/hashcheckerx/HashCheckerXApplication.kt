f226289228b969840990b982342d64c68bd7b347
fartem <jaman.smlnsk@gmail.com>
2025-08-04 23:05:43 +0300

    Removed unused imports (2025-08-04)

--- app/src/main/java/xyz/fartem/hashcheckerx/HashCheckerXApplication.kt
+++ app/src/main/java/xyz/fartem/hashcheckerx/HashCheckerXApplication.kt
@@ -3,7 +3,6 @@ package xyz.fartem.hashcheckerx
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import xyz.fartem.hashcheckerx.hash_generator.startup.HashGeneratorStartup
-import java.security.Security
 
 @HiltAndroidApp
 class HashCheckerXApplication : Application() {
